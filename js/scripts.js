bfb588f95ff4d2d1c31fc7beb45b18bab4c079f7
kyleines <kyleines@gmail.com>
2021-05-26 17:53:29 -0500

    complete modal with buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,25 @@ Treehouse FSJS Techdegree:
 Project 5 - Employee Directory
 ************************************************/
 
-const gallery = document.getElementById("gallery");
-const dataUrl = "https://randomuser.me/api/?exc=gender,login,registered,phone,id&results=12&nat=us";
 
+
+
+
+
+//------------------------------
+//               *
+//------------------------------
+const dataUrl = "https://randomuser.me/api/?exc=gender,login,registered,phone,id&results=12&nat=us";
+const gallery = document.getElementById("gallery");
+const body = document.querySelector("body");
+const script = document.querySelector("script");
+
+
+
+
+//------------------------------
+//          GALLERY
+//------------------------------
 async function getJSON(url) {
     try {
         const response = await fetch(url);
@@ -14,13 +30,11 @@ async function getJSON(url) {
         throw error;
     }
 }
-
 async function getEmployees(url) {
     const profiles = await getJSON(url)
                             .then(data => data.results);
     return profiles;
 }
-
 function generateHTML(data) {
     data.map(person => {
         const card = document.createElement("div");
@@ -43,24 +57,114 @@ function generateHTML(data) {
         card.appendChild(cardImage)
         card.appendChild(cardInfo)
         gallery.appendChild(card);
-    })
-
-    // console.log(data);
+    });
 }
 
+
+
+
+
+//------------------------------
+//           MODAL
+//------------------------------
+function getSelectedProfile(employees, selected, prev, next) {
+    for (let i = 0; i < employees.length; i++) {
+        const name = `${employees[i].name.first} ${employees[i].name.last}`;
+        if (name === selected) {
+            if (prev === true && i > 0) {
+                generateModal(employees[i - 1]);
+            } else if (next === true && i < employees.length - 1) {
+                generateModal(employees[i + 1]);
+            } else {
+                generateModal(employees[i]);
+            }
+        }
+    }
+}
+
+function generateModal(profile) {
+    const container = document.createElement("div");
+    container.className = "modal-container";
+
+    const modal = document.createElement("div");
+    modal.className = "modal";
+
+    const modalInfo = document.createElement("div");
+    modalInfo.className = "modal-info-container";
+    modal.insertAdjacentHTML("beforeend", `
+        <img class="modal-img" src="${profile.picture.large}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${profile.name.first} ${profile.name.last}</h3>
+        <p class="modal-text">${profile.email}</p>
+        <p class="modal-text cap">${profile.location.city}</p>
+        <hr>
+        <p class="modal-text">${profile.cell}</p>
+        <p class="modal-text">${profile.location.street.number} ${profile.location.street.name}., 
+                                ${profile.location.city}, 
+                                ${profile.location.state} ${profile.location.postcode}</p>
+        <p class="modal-text">${profile.dob.date}</p>
+    `);
+
+    const modalButtons = document.createElement("div");
+    modalButtons.className = "modal-btn-container";
+    modalButtons.insertAdjacentHTML("beforeend", `
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    `);
+
+    modal.appendChild(modalInfo);
+    modalInfo.insertAdjacentHTML("beforebegin", `
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    `);
+    container.appendChild(modal);
+    container.appendChild(modalButtons);
+    body.insertBefore(container, script);
+}
+
+function openModal(element) {
+    let name = "";
+        if (element.className === "card") {
+            name = element.children[1].firstElementChild.textContent;
+        } else if (element.parentElement.className === "card") {
+            name = element.parentElement.children[1].firstElementChild.textContent;
+        } else if (element.parentElement.parentElement.className === "card") {
+            name = element.parentElement.parentElement.children[1].firstElementChild.textContent;
+        }
+        return name;
+}
+function changeModal(element) {
+    let name = element.parentElement.previousSibling.children[1].textContent;
+    return name;
+}
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------
+//      EVENT LISTENERS
+//------------------------------
 window.addEventListener("load", async () => {
     const employees = await getEmployees(dataUrl);
-    generateHTML(employees)
+    generateHTML(employees);
+
+    body.addEventListener("click", (e) => {
+        getSelectedProfile(employees, openModal(e.target));
+        
+        if (e.target.tagName === "BUTTON" || e.target.parentElement.tagName === "BUTTON") {
+            document.getElementsByClassName("modal-container")[0].remove();
+
+            if (e.target.id === "modal-prev") {
+                getSelectedProfile(employees, changeModal(e.target), true, false);
+            }
+            if (e.target.id === "modal-next") {
+                getSelectedProfile(employees, changeModal(e.target), false, true);
+            }
+        }
+    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
